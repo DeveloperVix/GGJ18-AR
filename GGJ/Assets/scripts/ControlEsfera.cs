e4fad24b3687fbc5bf2cc80161de956c3a6dbb24
DeveloperVix <vicomisterio@hotmail.com>
2018-01-27 18:33:30 -0600

    :V
    :V

---
+++ GGJ/Assets/scripts/ControlEsfera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEsfera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NewPosition(GameObject casilla){
+		transform.position = new Vector3 (casilla.transform.position.x, casilla.transform.position.y, casilla.transform.position.z);
+	}
+
+	public void UpSelection()
+	{
+		Debug.Log ("Seleccion liberada");
+	}
+}
