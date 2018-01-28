b9ad407cc72eafcde8699ae99b475991a3a3426a
DeveloperVix <vicomisterio@hotmail.com>
2018-01-27 23:21:49 -0600

    Cambio de turno jugador mediante boton
    En el script GM se encuentra la funcion AcabaTurno, dicha funcion se manadara llamar en el boton de la UI para cambiar al siguiente jugador.

--- GGJ/Assets/scripts/GM.cs
+++ GGJ/Assets/scripts/GM.cs
@@ -5,39 +5,35 @@ using UnityEngine.UI;
 
 public class GM : MonoBehaviour {
 
-    public GameObject[] players;
-
-    public int tiempoTurno = 15;
-
-
-    [Header("UI")]
-    public Text tiempoTurnoTxt;
+    public GameObject[] players;    //Para saber quien esta jugando
 
     [Header("Botones")]
     public GameObject btnInicio;
 
 
-    public void IniciaJuego()
-    {
+    int jugadorPasado;
+    int jugadorEnTurno;
 
+    // Use this for initialization
+    void Start () {
+         jugadorPasado = 0;    //necesito asignarle el jugador que comenzo aleatoriamente
     }
-
-    IEnumerator TimeGame()
-    {
-        while (tiempoTurno > 0)
-        {
-            yield return new WaitForSeconds(1f);
-            tiempoTurno--;
-        }
-    }
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void AcabaTurno()    //Para el boton de "siguiente turno"
+    {
+        jugadorEnTurno = jugadorPasado + 1;
+
+        if (jugadorEnTurno > players.Length - 1)
+        {
+            jugadorEnTurno = 0;
+        }
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[jugadorPasado].GetComponent<JugadorTurno>().turno = false;
+        }
+
+        players[jugadorPasado].GetComponent<JugadorTurno>().turno = true;
+    }
 }
---
+++ GGJ/Assets/scripts/JugadorTurno.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorTurno : MonoBehaviour {
+
+    public bool turno = false;
+
+    public int noMovimientos = 1;
+
+}
